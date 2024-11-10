608cea66ecb5480a7086d3aba297dd532433cdac
Frederik Göbel <frederikgoebel@gmx.de>
2024-11-10 16:10:18 +0100

    Moved to palette

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,10 @@
 use std::thread;
 use std::time::Duration;
 
-use color_spaces::OkLab;
-use color_spaces::OkLch;
-use color_spaces::SRGB;
 use esp_idf_svc::hal::gpio::PinDriver;
 use esp_idf_svc::hal::prelude::Peripherals;
+use palette::IntoColor;
 use ws2812_esp32_rmt_driver::driver::Ws2812Esp32RmtDriver;
-
-mod color_spaces;
 
 #[no_mangle]
 fn main() {
@@ -24,11 +20,7 @@ fn main() {
 
     let audio_gate = PinDriver::input(peripherals.pins.gpio6).unwrap();
 
-    let mut color = OkLch {
-        l: 1.0,
-        c: 0.34,
-        h: 0.0,
-    };
+    let mut color = palette::Okhsv::new(0.0, 1.0, 1.0);
 
     let mut energy: f32 = 0.0;
     loop {
@@ -38,15 +30,13 @@ fn main() {
         } else {
             energy += 0.02;
             energy = energy.clamp(0.0, 1.0);
-            color.h += 1.0;
-            color.h %= 360.0;
+            color.hue += 0.7;
         }
-        color.l = energy;
-        let oklab: OkLab = color.into();
-        let srgb: SRGB = oklab.into();
-        let mut r = (srgb.r * 255.0) as u8;
-        let mut g = (srgb.g * 255.0) as u8;
-        let mut b = (srgb.b * 255.0) as u8;
+        color.value = energy;
+        let srgb: palette::Srgb = color.into_color();
+        let mut r = (srgb.red * 255.0) as u8;
+        let mut g = (srgb.green * 255.0) as u8;
+        let mut b = (srgb.blue * 255.0) as u8;
         if energy == 0.0 {
             r = 0;
             g = 0;
